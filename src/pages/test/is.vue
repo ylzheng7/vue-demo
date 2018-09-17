89c4f73e26e58a2b14a4d236c735c9fc97d5061a
desunire <desunire@163.com>
2018-09-17 11:14:16 +0800

    first commit

---
+++ src/pages/test/is.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div id="main-body">
+        <table>
+            <tbody>
+            <tr>1</tr>
+            <tr>2</tr>
+            <tr>3</tr>
+            </tbody>
+        </table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "is",
+        // component:this.component('row',{
+        //    template:'<tr><td>this is a td row</td></tr>'
+    // }),
+    }
+</script>
+
+<style scoped>
+
+</style>
